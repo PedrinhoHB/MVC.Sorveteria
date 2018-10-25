57690cc6d9eec41b5332e4eaa4001da8e3803e64
Pedrinho <pedro.bizzarri@sesisp.org.br>
2018-10-25 11:58:57 -0300

    Criacao da classe de Persistencia

---
+++ MVC.Sorveteria/Persistencia/SorveteriaContext.cs
@@ -0,0 +1,15 @@
+﻿using MVC.Sorveteria.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Sorveteria.Persistencia
+{
+    public class SorveteriaContext : DbContext
+    {
+        public DbSet<Sorvete> Sorvetes { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+    }
+}
