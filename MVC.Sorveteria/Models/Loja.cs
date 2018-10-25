0acab3d0fd7444488f3b428cb9634dd88e2363e5
Pedrinho <pedro.bizzarri@sesisp.org.br>
2018-10-25 11:56:13 -0300

    Adicao das classe do Models

---
+++ MVC.Sorveteria/Models/Loja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Sorveteria.Models
+{
+    public class Loja
+    {
+        public int LojaId { get; set; }
+        public string Nome { get; set; }
+        public string Local { get; set; }
+
+        //Relacionamento
+        public IList<Sorvete> Sorvetes { get; set; }
+    }
+}
---
+++ MVC.Sorveteria/Models/Sorvete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Sorveteria.Models
+{
+    public class Sorvete
+    {
+        public int SorveteId { get; set; }
+        public string Nome { get; set; }
+        public string Sabor { get; set; }
+        public DateTime DataValidade { get; set; }
+        public bool Casquinha { get; set; }
+
+        //Relacionamento
+        public Loja Loja { get; set; }
+        public int LojaId { get; set; }
+    }
+}
