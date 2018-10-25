66547be9ab9e82a21c80f607f4ed7e34bcbb2717
Pedrinho <pedro.bizzarri@sesisp.org.br>
2018-10-25 13:16:39 -0300

    Criacao da classe HomeController

---
+++ MVC.Sorveteria/Controllers/SorveteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Sorveteria.Controllers
+{
+    public class SorveteController : Controller
+    {
+        // GET: Sorvete
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
