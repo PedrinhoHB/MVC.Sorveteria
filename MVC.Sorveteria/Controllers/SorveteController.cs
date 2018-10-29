4b7ac4eda9e43d26b1de417c245b52c1fa2087a7
Pedrinho <pedro.bizzarri@sesisp.org.br>
2018-10-29 16:59:50 -0300

    Criacao da pagina para cadastrar um sorvete

--- MVC.Sorveteria/Controllers/SorveteController.cs
+++ MVC.Sorveteria/Controllers/SorveteController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVC.Sorveteria.Models;
+using MVC.Sorveteria.Units;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,27 @@ namespace MVC.Sorveteria.Controllers
 {
     public class SorveteController : Controller
     {
-        // GET: Sorvete
-        public ActionResult Index()
+        private UnityOfWork _unit = new UnityOfWork();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Sorvete sorvete)
+        {
+            _unit.SorveteRepository.Cadastrar(sorvete);
+            _unit.Salvar();
+            TempData["msg"] = "Sorvete cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _unit.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
